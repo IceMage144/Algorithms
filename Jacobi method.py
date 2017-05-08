b55efee4c17a583c175f2c93762842ca33d5ca09
Zafreeti <jgabriel.basi@hotmail.com>
2017-05-08 20:57:25 -0300

    Adding Jacobi method solver and Mandelbrot set visualizer

---
+++ Jacobi method.py
@@ -0,0 +1,105 @@
+import math
+
+def norm(mat):
+    s = 0
+    for i in mat:
+        for j in i:
+            s += j**2
+    return math.sqrt(s)
+
+def multMat(mat1, mat2):
+    ret = [[0 for i in range(len(mat2[0]))] for j in range(len(mat1))]
+    for i in range(len(mat1)):
+        for k in range(len(mat2[0])):
+            s = 0
+            for j,l in zip(range(len(mat1[0])), range(len(mat2))):
+                s += mat1[i][j]*mat2[l][k]
+            ret[i][k] = s
+    return ret
+
+def invDiag(mat):
+    ret = [[0 for i in range(len(mat))] for j in range(len(mat))]
+    [[0]*len(mat)]*len(mat)
+    for i in range(len(mat)):
+        ret[i][i] = (0 if mat[i][i] == 0 else 1/mat[i][i])
+    return ret
+
+def diagRemainder(mat):
+    ret = [[0 for i in range(len(mat))] for j in range(len(mat))]
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            ret[i][j] = (0 if i == j else mat[i][j])
+    return ret
+
+def subMat(mat1, mat2):
+    ret = [[0 for i in range(len(mat1[0]))] for j in range(len(mat1))]
+    for i in range(len(mat1)):
+        for j in range(len(mat1[0])):
+            ret[i][j] = mat1[i][j] - mat2[i][j]
+    return ret
+
+def main():
+    '''A = [[10, -1, 2, 0],
+         [-1, 11, -1, 3],
+         [2, -1, 10, -1],
+         [0, 3, -1, 8]]
+    b = [[6], [25], [-11], [15]]
+    x = [[0], [0], [0], [0]]'''
+    n = int(input("Input the number of variables:\n"))
+    err = float(input("Input the precision of the algorithm:\n"))
+    m = int(input("Input the max number of iterations:\n"))
+    A = [[0 for i in range(n)] for j in range(n)]
+    print("=== Starting matrix A ===")
+    for i in range(n):
+        for j in range(n):
+            A[i][j] = float(input(f"Enter a{i}{j}: "))
+    print("=== Finished matrix A ===")
+    b = [[0] for i in range(n)]
+    print("=== Starting b vector ===")
+    for i in range(n):
+        b[i][0] = float(input(f"Enter b{i}: "))
+    print("=== Finished b vector ===")
+    x = [[0] for i in range(n)]
+    print("=== Starting guess vector ===")
+    for i in range(n):
+        x[i][0] = float(input(f"Enter x{i}: "))
+    print("=== Finished guess vector ===")
+    counter = 0
+    xMem = [[0] for i in range(n)]
+    print(f"x{counter}:", x)
+    xMem = x.copy()
+    DInv = invDiag(A)
+    #print("DInv:", DInv)
+    R = diagRemainder(A)
+    #print("R:", R)
+    T = multMat(DInv, R)
+    #print("T:", T)
+    C = multMat(DInv, b)
+    #print("C:", C)
+    M = multMat(T, xMem)
+    #print("M:", M)
+    x = subMat(C, M)
+    s = abs(norm(x)-norm(xMem))
+    counter += 1
+    while (counter < m and s > err):
+        print("=============================================")
+        print(f"x{counter}:", x)
+        xMem = x.copy()
+        DInv = invDiag(A)
+        #print("DInv:", DInv)
+        R = diagRemainder(A)
+        #print("R: ", R)
+        T = multMat(DInv, R)
+        #print("T: ", T)
+        C = multMat(DInv, b)
+        #print("C: ", C)
+        x = subMat(C, multMat(T, xMem))
+        s = abs(norm(x)-norm(xMem))
+        counter += 1
+    print("Result: ", x)
+    for i in range(len(x)):
+        x[i][0] = round(x[i][0], 4)
+    print("Rounded answer: ", x)
+
+if __name__=="__main__":
+    main()
---
+++ MandelbrotSet/main.cpp
@@ -0,0 +1,109 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+#define PIXEL_SIZE 2
+#define W 800
+
+class IPoint {
+    sf::RectangleShape rect;
+public:
+    IPoint() {
+        rect = sf::RectangleShape();
+    }
+    IPoint(sf::Vector2f pos, sf::Color col) {
+        rect = sf::RectangleShape();
+        rect.setFillColor(col);
+        rect.setPosition(pos);
+        rect.setSize(sf::Vector2f(PIXEL_SIZE, PIXEL_SIZE));
+    }
+    void draw(sf::RenderWindow& window) {
+        window.draw(rect);
+    }
+};
+
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(W, W), "Mandelbrot set");
+    sf::VertexArray window_bg(sf::Quads, 4);
+    sf::View view1(sf::FloatRect(-W, -W, 2*W, 2*W));
+    vector<sf::Color> croma;
+    //vector<IPoint> points;
+    IPoint point;
+    vector<float> val;
+    float ptrx = -W, ptry = -W;
+    int n = 0;
+    bool plot = true;
+
+    // Setting window background color
+    //window_bg[0].position = sf::Vector2f(-W, -W);
+    //window_bg[1].position = sf::Vector2f(W, -W);
+    //window_bg[2].position = sf::Vector2f(W, W);
+    //window_bg[3].position = sf::Vector2f(-W, W);
+
+    //for (int i = 0; i < 4; i++)
+    //    window_bg[i].color = sf::Color(0, 0, 0);
+
+    //window.draw(window_bg);
+
+    croma.push_back(sf::Color(0, 0, 255));
+    for (int i = 0; i < 255; i += 15)
+        croma.push_back(sf::Color(i, i, 255));
+    for (int i = 255; i > 0; i -= 15)
+        croma.push_back(sf::Color(255, 255, i));
+    for (int i = 255; i > 0; i -= 15)
+        croma.push_back(sf::Color(i, i, 0));
+
+    window.setView(view1);
+
+    val.resize(2);
+    val[0] = 0.0;
+    val[1] = 0.0;
+
+    while (window.isOpen()) {
+        sf::Event event;
+
+        // Events handling
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+        }
+
+        if (plot) {
+            int iterr = 0;
+            float wptrx = 2*ptrx/W;
+            float wptry = 2*ptry/W;
+            float dist = 0.0;
+            float tmp = 0.0;
+            val[0] = 0.0;
+            val[1] = 0.0;
+            while (dist < 4 && iterr <= 51) {
+                tmp = val[0]*val[0] - val[1]*val[1] + wptrx;
+                val[1] = 2*val[0]*val[1] + wptry;
+                val[0] = tmp;
+                dist = val[0]*val[0] + val[1]*val[1];
+                iterr++;
+            }
+            point = IPoint(sf::Vector2f(ptrx, ptry), croma[iterr]);
+            //points.push_back(IPoint(sf::Vector2f(ptrx, ptry), croma[iterr]));
+            if (ptrx > W && ptry > W)
+                plot = !plot;
+            ptrx += PIXEL_SIZE;
+            if (ptrx > W) {
+                ptrx = -W;
+                ptry += PIXEL_SIZE;
+            }
+            n++;
+        }
+
+        //window.clear();
+        //for (int i = 0; i < n; i++)
+        //points[n-1].draw(window);
+        point.draw(window);
+        window.display();
+    }
+    return 0;
+}
